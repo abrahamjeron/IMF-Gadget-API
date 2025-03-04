7f97756fd6cc6808c0334b9681d61af788a750d1
abrahamjeron <abrahamjeron40@gmail.com>
2025-03-04 11:39:42 +0530

    hidden the port number in env file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config(); 
 const gadgetRoutes = require('./routes/gadgetRoutes');
 const authRoutes = require('./routes/authRoutes');
 const { sequelize } = require('./config/database');
-
+const port = process.env.PORT
 const app = express();
 
 app.use(cors());
@@ -16,6 +17,6 @@ app.use('/auth', authRoutes);
 sequelize.authenticate()
     .then(() => {
         console.log('Connected to Supabase PostgreSQL ✅');
-        app.listen(3001, () => console.log('Server running on port 3001'));
+        app.listen(port, () => console.log('Server running on port 3001'));
     })
     .catch(err => console.error('Database connection error ❌', err))
